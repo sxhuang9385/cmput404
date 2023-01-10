0f14b015c15b4d951713f7ee3670febb5f27f70c
J <sxhuang@ualberta.ca>
2023-01-09 18:26:41 -0700

    :D

--- find_version.py
+++ find_version.py
@@ -2,6 +2,6 @@ import requests
 
 print(requests.__version__)
 print(requests.get("http://www.google.com"))
-r = requests.get()
+r = requests.get("https://raw.githubusercontent.com/sxhuang9385/cmput404/master/find_version.py")
 
 print(r.text)
